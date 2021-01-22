3f4795fdb14051ff95671d744a9087f1fea1ed39
Diana Liao <dijali@gmail.com>
2021-01-22 05:54:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
   
-  long_planeteer_calls.
+  long_planeteer_calls.any? do |
 end
 
 def find_valid_calls(planeteer_calls)
